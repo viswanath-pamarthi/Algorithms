4cc287ceedc711c69f9be9dc04617752b8e75dd0
Viswanath Pamarthi <viswanath.pamarthi@outlook.com>
2017-05-30 00:45:03 -0500

    Updates

---
+++ LeetCode/ArrayPartition-I.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/// <summary>
+/// https://leetcode.com/problems/array-partition-i/#/description
+/// Given an array of 2n integers, your task is to group these integers into n pairs of integer, say (a1, b1), (a2, b2), ..., (an, bn) which makes sum of min(ai, bi) for all i from 1 to n as large as possible.
+///Example 1:
+///Input: [1,4,3,2]
+///
+///Output: 4 (maximunm possible sum by adding  min(each pair)
+///Explanation: n is 2, and the maximum sum of pairs is 4.
+///Note:
+///n is a positive integer, which is in the range of[1, 10000].
+///All the integers in the array will be in the range of[-10000, 10000].
+/// </summary>
+namespace LeetCode
+{
+    class ArrayPartition_I
+    {
+        /// <summary>
+        /// sort the numbers in array and make the consecutive number in the array pair to make the maximm possible sum
+        ///  Question ...maximunm possible sum by adding  min(each pair)
+        /// https://discuss.leetcode.com/topic/87206/java-solution-sorting-and-rough-proof-of-algorithm
+        /// </summary>
+        /// <param name="nums"></param>
+        /// <returns></returns>
+        public int ArrayPairSum(int[] nums)
+        {
+            int result=0;
+            
+            //Sort the numbers in array
+            Array.Sort(nums);
+
+            //As the consecutive numbers in array are paired, the alternative numbers in the array(smallest number in each pair) are added
+            for (int i = 0; i < nums.Length; i += 2)
+            {
+                result += nums[i];
+            }
+            
+            return result;
+        }
+    }
+}
---
+++ LeetCode/TwoSumII.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/// <summary>
+/// https://leetcode.com/problems/two-sum-ii-input-array-is-sorted/#/description
+/// Given an array of integers that is already sorted in ascending order, find two numbers such that they add up to a specific target number.
+///
+///The function twoSum should return indices of the two numbers such that they add up to the target, where index1 must be less than index2.Please note that your returned answers(both index1 and index2) are not zero-based.
+///
+///You may assume that each input would have exactly one solution and you may not use the same element twice.
+///
+///Input: numbers={ 2, 7, 11, 15}, target=9
+///Output: index1=1, index2=2
+/// </summary>
+namespace LeetCode
+{
+    class TwoSumII
+    {
+        public int[] TwoSum(int[] numbers, int target)
+        {
+            int[] result = new int[2] { 0, 0 };
+            int i, j;
+            i = 0;
+            j = numbers.Length - 1;
+
+            while ((numbers[i] + numbers[j] != target) &&(i<j))
+            {
+                if (numbers[i] + numbers[j] < target)
+                {
+                    i++;
+                }
+                else
+                {
+                    j--;
+                }
+              
+            }
+
+            if(i>=j)
+            {
+                result[0] = 0;
+                result[1] = 0;
+
+                return result;
+            }
+            else
+            {
+                result[0] = i + 1 ;
+                result[1] = j + 1;
+            }
+            return result;
+        }
+    }
+}
