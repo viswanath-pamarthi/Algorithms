efc0532d9fb41c1e820c2159100795c79467d8ba
Viswanath Pamarthi <viswanath.pamarthi@outlook.com>
2017-09-22 19:18:12 -0500

    Updates 9/22

--- LeetCode/KeyboardRow.cs
+++ LeetCode/KeyboardRow.cs
@@ -4,6 +4,22 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+/// <summary>
+/// https://leetcode.com/problems/keyboard-row/description/
+/// Given a List of words, return the words that can be typed using letters of alphabet on only one row's of American keyboard like the image below.
+///
+///
+///American keyboard
+///
+///
+///Example 1:
+///Input: ["Hello", "Alaska", "Dad", "Peace"]
+///Output: ["Alaska", "Dad"]
+///Note:
+///You may use one character in the keyboard more than once.
+///You may assume the input string will only contain letters of alphabet.
+///
+/// </summary>
 namespace LeetCode
 {
     class KeyboardRow
