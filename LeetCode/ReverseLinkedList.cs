2058a4a60f5b2988d65f0e3b2a16368d9cde918e
Viswanath Pamarthi <viswanath.pamarthi@outlook.com>
2017-10-05 15:45:32 -0500

    Reverse Linked List

---
+++ LeetCode/ReverseLinkedList.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/// <summary>
+/// https://leetcode.com/problems/reverse-linked-list/description/
+/// Reverse a singly linked list.
+/// </summary>
+namespace LeetCode
+{
+    class ReverseLinkedList
+    {
+        public ListNode ReverseList(ListNode head)
+        {
+
+            if (head == null)
+                return head;
+
+            //new head of the list (the tail)
+            ListNode newHead = new ListNode(0);
+
+            Reverse(head, ref newHead);
+
+            return newHead;
+
+        }
+
+        public ListNode Reverse(ListNode head, ref ListNode newHead)
+        {
+            if (head.next == null)
+            {
+                newHead = new ListNode(head.val);
+                newHead.next = null;
+                return newHead;
+            }
+
+            ListNode temp = Reverse(head.next, ref newHead);
+
+            temp.next = head;
+            head.next = null;
+
+            return head;
+        }
+    }
+}
---
+++ LeetCode/ReverseLinkedListII.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode
+{
+    class ReverseLinkedListII
+    {
+        public ListNode ReverseBetween(ListNode head, int m, int n)
+        {
+
+            ListNode newMthNode = new ListNode(0);
+            ListNode temp = head;
+
+            ListNode nPlusOneNode = new ListNode(0);
+
+            //navigate till mth node
+            for (int i = 1; i < m - 1; i++)
+            {
+                temp = temp.next;
+            }
+
+            if(m==1)
+            {
+                Reverse(temp, ref newMthNode, ref nPlusOneNode, m, n);
+                temp.next = nPlusOneNode;
+                temp = newMthNode;
+                head = temp;
+            }
+            else
+            {
+                Reverse(temp.next, ref newMthNode, ref nPlusOneNode, m, n);
+                temp.next.next = nPlusOneNode;
+                temp.next = newMthNode;
+            }
+            
+
+            return head;
+        }
+
+        public ListNode Reverse(ListNode head, ref ListNode newMthNode, ref ListNode nPlusOneNode, int m, int n)
+        {
+            if (m == n)
+            {
+                newMthNode = head;
+                nPlusOneNode = head.next;
+                newMthNode.next = null;
+
+                return newMthNode;
+            }
+
+            ListNode temp = Reverse(head.next, ref newMthNode, ref nPlusOneNode, m + 1, n);
+
+            temp.next = head;
+            head.next = null;
+
+            return head;
+
+        }
+    }
+}
